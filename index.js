75e118cdbe069f96bca80404ccdcc6f39df9324c
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2018-01-25 11:35:46 +0530

    Scoped UMD.

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-const noop = () => {}
-const updateStates = ({
-  beginForward = noop, completeForward = noop, runForward = noop, // Only triggers for forward
-  beginReverse = noop, completeReverse = noop, runReverse = noop, // and reversed animation.
-  begin        = noop, complete        = noop, run        = noop, // Equivalent to built-ins, but completes every time.
-                      }) => (a) => {                              // Returns a callback for built-in `update`.
-  if (!a.reversed) {
-    if (a.progress == 0) {
-      begin(a)
-      beginForward(a)
-    }
-    if (a.progress == 100) {
-      completeForward(a)
-      complete(a)
-    }
-    runForward(a)
-  }   
-  run(a)
-  if (a.reversed) {      
-    runReverse(a)
-    if (a.progress == 100) {
-      begin(a)
-      beginReverse(a)
-    }
-    if (a.progress == 0) {
-      completeReverse(a)
-      complete(a)
-    }
-  }
-}
-
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         define([], factory);
@@ -37,4 +6,35 @@ const updateStates = ({
     } else {
         root.updateStates = factory();
   }
-}(typeof self !== 'undefined' ? self : this, () => updateStates));
+}(typeof self !== 'undefined' ? self : this, () => {
+  const noop = () => {}
+  return ({
+    beginForward = noop, completeForward = noop, runForward = noop, // Only triggers for forward
+    beginReverse = noop, completeReverse = noop, runReverse = noop, // and reversed animation.
+    begin        = noop, complete        = noop, run        = noop, // Equivalent to built-ins, but completes every time.
+                        }) => (a) => {                              // Returns a callback for built-in `update`.
+    if (!a.reversed) {
+      if (a.progress == 0) {
+        begin(a)
+        beginForward(a)
+      }
+      if (a.progress == 100) {
+        completeForward(a)
+        complete(a)
+      }
+      runForward(a)
+    }   
+    run(a)
+    if (a.reversed) {      
+      runReverse(a)
+      if (a.progress == 100) {
+        begin(a)
+        beginReverse(a)
+      }
+      if (a.progress == 0) {
+        completeReverse(a)
+        complete(a)
+      }
+    }
+  }
+}))
